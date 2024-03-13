de91f4666aa84ca723a9169670407651f7996176
Shishir-rahman <sarkershishir4@gmail.com>
2024-03-13 23:19:46 +0600

    JS

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var num1 = 5;
+var num2 = 3;
+
+var sum, sub;
+sum = num1 + num2;
+sub = num1 -num2
+document.getElementById("test").innerHTML = sum;
+
+document.getElementById("test").innerHTML = sub;
