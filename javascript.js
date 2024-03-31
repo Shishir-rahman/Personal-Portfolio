a84388e2e8668dbed48a98c77cecf19c09d86a24
Shishir-rahman <sarkershishir4@gmail.com>
2024-04-01 00:16:48 +0600

    add image slide

--- javascript.js
+++ javascript.js
@@ -1,5 +1,29 @@
+let image = ["./Image/IMG-20220303-WA0074-01.jpeg", "./Image/1701956868286-01.jpeg", "./Image/1698561709365-01.jpeg"];
+let imgTag = document.getElementById("section2-pic");
+let count = 0;
 
-["Hello", "Shishir"].forEach(alert);
 
-const birthday = '17.06.1997';
-const age = someCode(birthday);
+function next(){
+    count++;
+
+    if(count >= image.length){
+        count = 0;
+        imgTag.src = image[count];
+    }
+    else{
+        imgTag.src = image[count];
+    }
+}
+
+
+function prev(){
+    count--;
+
+    if(count < 0){
+        count = image.length - 1;
+        imgTag.src = image[count];
+    }
+    else{
+        imgTag.src = image[count];
+    }
+}
